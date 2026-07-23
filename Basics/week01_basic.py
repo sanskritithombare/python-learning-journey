804e67e5c9095107c468c73ceb00203f9cdda9d2
sanskritithombare <91877829+sanskritithombare@users.noreply.github.com>
2026-07-23 14:21:02 +0100

    Basics python
    Basics with print

---
+++ Basics/week01_basic.py
@@ -0,0 +1,25 @@
+print("Hello World")
+
+print("This shall too pass")
+
+print(4 + 7)
+
+print(10 - 7)
+
+print(4*5)
+
+print(10 % 3) #Remainder which is 1
+
+print(20 / 4)
+
+print((2 + 3) * 5)  #25 Code using parentheses to change the order of operations
+
+#Code performing multiplication first due to operator precedence
+print(2 + 3 * 5)  # 17, not 25
+
+#Age in days
+print(31*365)
+
+name = "sans" # Add/change your name here
+age = 31 # Add/change your age here
+print("Hi, my name is", name, "and I’m", age, "years old.")
