361935ebe9882f7e53aad449be76ba3392495918
sanskritithombare <91877829+sanskritithombare@users.noreply.github.com>
2026-08-04 16:58:28 +0100

    challenge for conditions and error handling

--- Basics/conditional_flow.py
+++ Basics/conditional_flow.py
@@ -8,10 +8,21 @@ else:
 
 #if, elseif, else
 hour = 14
-
 if hour < 12:
     print("Good morning!")
 elif hour < 18:
     print("Good afternoon!")
 else:
     print("Good evening!")
+
+
+#Challenge: Movie Night
+
+#Ask the user for their age. If they’re 13 or older, they can watch a movie. Otherwise, say they’re too young.
+
+age = int(input("How old are you? "))
+
+if age >= 13:
+    print("You can watch the movie!")
+else:
+    print("You're too young to watch movie!")
---
+++ Basics/errorhandling.py
@@ -0,0 +1,10 @@
+
+try:
+ number_1 = int(input("Type the 1st number "))
+ number_2 = int(input("Type the 2nd number "))
+
+
+ print(" The division is: ", number_1 / number_2 )
+
+except:
+ print("Invalid number")
