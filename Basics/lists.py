c7515dcd7996eb28c51e7f6365a290f9b1413bb3
sanskritithombare <91877829+sanskritithombare@users.noreply.github.com>
2026-08-26 13:36:24 +0100

    Create lists.py

---
+++ Basics/lists.py
@@ -0,0 +1,11 @@
+#create a list 
+fruits = ["apple", "banana", "cherry"]
+print(fruits)
+
+#Printing the first and third items in a list
+print(fruits[0])  # First item
+print(fruits[2])  # Third item
+
+fruits.append("orange")  # Add
+fruits.remove("banana")  # Remove
+print(fruits)
