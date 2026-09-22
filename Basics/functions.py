fc1d1c5a5a11aaa858aa06a983470b3c23d9a1f7
sanskritithombare <91877829+sanskritithombare@users.noreply.github.com>
2026-09-22 11:22:13 +0100

    Challenge

--- Basics/functions.py
+++ Basics/functions.py
@@ -38,3 +38,21 @@ def adventure():
         print("They ignore us. We float alone forever.")
 
 adventure()
+
+#Challenge
+# Modify the function below by adding more layers to make the story your own!
+def jungle_adventure():
+    # Ask the user to choose between two paths
+    path = input("You find two paths: one goes to a river, the other to a mountain. Where do you go? ")
+
+    # Check what the user typed and respond accordingly
+    if path == "river":
+        print("You swim with dolphins!")
+    elif path == "mountain":
+        print("You find an ancient temple!")
+    else:
+        # If the user typed something else, give this outcome
+        print("You wander into the jungle and get lost.")
+
+# Start the jungle adventure by calling the function
+jungle_adventure()
